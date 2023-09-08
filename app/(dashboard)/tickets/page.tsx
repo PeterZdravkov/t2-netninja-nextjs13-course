0c8bbf7de79ed4e36d9744f98c998d79be83f328
PeterZdravkov <peter.geotchliev@gmail.com>
2023-09-08 04:21:40 +0300

    deploy to vercel

--- app/(dashboard)/tickets/page.tsx
+++ app/(dashboard)/tickets/page.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import TicketList from "./TicketList";
 import Loading from "@/app/(dashboard)/loading";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Helpdesk | Tickets",
@@ -18,6 +19,9 @@ const Tickets = () => {
             <small>Currently open tickets</small>
           </p>
         </div>
+        <Link href={"/tickets/create"} className="ml-auto mt-1">
+          <button className="btn-primary">Create a Ticket</button>
+        </Link>
       </nav>
       <Suspense fallback={<Loading />}>
         <TicketList />
